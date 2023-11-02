3c0a1279da95d51f2582180d3dbc2da37c2ff278
yani93anto <cammarasana.yanina@gmail.com>
2023-11-01 23:27:04 -0300

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import { Form } from "./Components/Form";
 import { Historial } from "./Pages/Historial";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// Componente Principal que contiene Header y Form
+function Principal() {
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  );
+}
+
 function App() {
   return (
     <>
       <Router>
-        {/* NAVEGADOR */}
-        <Header />
-
-        {/* FORMULARIO */}
-        <Form />
         <Routes>
-          <Route path="./Pages/Historial" element={<Historial />} />
+          {/* Ruta principal */}
+          <Route path="/" element={<Principal />} />
+
+          {/* Ruta para el historial */}
+          <Route path="/historial" element={<Historial />} />
         </Routes>
       </Router>
     </>
